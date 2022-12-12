b3a5924e6e0146c0b0e8a0a5dfdfa741e92f7544
Orbitadoo <juanpaavaldezz@gmail.com>
2022-12-12 20:04:15 -0300

    end

--- js/main.js
+++ js/main.js
@@ -7,13 +7,25 @@ const cartValue = document.getElementById("cart-value");
 // CARRITO
 const cart = document.getElementById("cart");
 // PRODUCTOS
-const productos = [
-    "Remera", 
-    "Pantalón", 
-    "Campera", 
-    "Buzo"
-];
-const compra = [];
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+        this.vendido = false;
+    }
+    
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+    }
+}
+
+const productos = [];
+productos.push(new Producto("Remera", "120"));
+productos.push(new Producto("Pantalón", "150"));
+productos.push(new Producto("Buzo", "200"));
+productos.push(new Producto("Campera", "250"));
+
+const carritoUsuario = [];
 
 let numero = 0;
 
@@ -32,16 +44,43 @@ restar.addEventListener("click", ()=>{
 // BUTTON ADD TO CART
 const addCart = document.getElementById("add-cart");
 addCart.addEventListener("click", ()=>{
-    cartValue.innerHTML = numero;
     for (let i = 0; i < 3; i++) {
+        cartValue.innerHTML = numero;
         if (i < 1) {
-            let nombre = prompt("Estos son los productos disponibles: ");
-            console.log(nombre);
+            let nombre = prompt("Ingrese su Nombre");
+            if (nombre.length >= 3) {
+                let compraUsuario = prompt("Ingrese el nombre del producto que desea comprar \n" + " Remera \n Pantalón \n Buzo \n Campera");
+                carritoUsuario.push(compraUsuario);
+            } else {
+                alert("Por favor, ingrese su nombre.");   
+            }
         } else if (i < 2) {
-            let compra = prompt("¿Qué quieres comprar?");
-        } else if (i < 3) {
-            let confirmacion = confirm("¿Deseas añadir esta cantidad al carrito?");
+            for (const producto of productos) {
+                producto.sumaIva();
+            }
         }
     }
 });
 
+cart.addEventListener("click", ()=>{
+    if (carritoUsuario.length == 0) {
+        alert("Usted no tiene elementos en el carrito")
+    } else {
+        let decision = confirm("Usted tiene en el carrito " + carritoUsuario + "\n¿Desea eliminar estos elementos?");
+        if (decision == true) {
+            carritoUsuario.pop();
+            alert("Se borraron los elementos del carrito");
+            location.reload();
+        } else {
+            alert("Puede continuar comprando");
+        }
+    }
+})
+//FUNCION ELIMINAR OBJETOS DEL CARRITO
+const eliminar = (nombre) => {
+    let index = carritoUsuario.indexOf(nombre)
+
+    if (carritoUsuario != -1) {
+        carritoUsuario.splice(index, 1)
+    }
+}
